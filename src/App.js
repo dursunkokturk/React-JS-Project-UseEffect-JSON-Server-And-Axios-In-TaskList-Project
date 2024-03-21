a4358a5fac67dced66062e2b839b92ca08370b42
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-03-21 14:35:52 +0300

    useEffect-json-server-and-axios-in-tasklist-project

--- src/App.js
+++ src/App.js
@@ -1,147 +1,60 @@
 import './App.css';
-
-// Task Create Sablonunu Ekrana Yazdirmak Icin
-// TaskCreate.js Dosyasini Import Ediyoruz
 import TaskCreate from './components/TaskCreate';
-
-// Task Listesini Yazdirmak Icin
-// TaskList.js Dosyasini Import Ediyoruz
 import TaskList from './components/TaskList';
 import { useState } from 'react';
-
-// HTTP Istemcisini Import Ediyoruz
 import Axios from 'axios';
-
-// Form Uzerinden Girilen Data nin
-// Sayfa Yenilendigi Anda Kaybolmasini Onlemek Icin
-// useEffect Componentini Kullaniyoruz
 import { useEffect } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  /* TaskCreate.js Dosyasinda 
-      handleSubmit Fonksiyonundaki
-      title ve taskDecription Degiskenlerindeki Degerleri Aliyoruz */
   const createTask = async (title, taskDescription) => {
-
-    // Database e Baglaniyoruz
     const response = await Axios.post('http://localhost:3004/tasks', {
-
-      // id Data si Otomatik Olarak Verilmesini Istedigimiz Icin
-      // id Parametresini Yazmiyoruz
-      // Database Hangi Data larin Gonderilecegini Belirtiyoruz
       title,
       taskDescription,
     });
     console.log(response);
-
-    // Olusturulacak Task Icin Gerekli Bilgileri Duzenliyoruz
-    // Ayni Zamanda Girilen Her Task Bilgisini Ve 
-    // En Son Girilen Task Bilgisini Tutuyoruz
     const createdTasks = [
-      
-      // Olusturulan Task Icin 
-      // Ilk Olarak Array Tipi Oldugunu Belirtiyoruz
       ...tasks, 
       response.data
     ];
-
-    // Girilen Tum Task Bilgilerini Aliyoruz
-    // Ve Bu Fonksiyon Icindeki tasks Degiskenine Atama Yapiyoruz
     setTasks(createdTasks);
   };
-
-  // Data nin Alinacagi Database e Baglaniyoruz
   const fetchTasks = async () => {
     const response = await Axios.get('http://localhost:3004/tasks');
     debugger;
     setTasks(response.data);
   };
-
-  // Var Data yi Database den Aliyoruz
   useEffect(() => {
-
-    // Database Icindeki Data yi 
-    // fetchTasks Metodu Ile Cagiriyoruz
     fetchTasks();
-
-    // App Componentinin Sadece Baslangicta Gorunmesi Icin
-    // Bos Array Olusturuyoruz
   }, []);
-
-  // Eklenen Task lar Icinde id Parametresi Uzerinden
-  // Task Bilgisini Aliyoruz
   const deleteTaskById = async (id) => {
     await Axios.delete(`http://localhost:3004/tasks/${id}`);
-
-    // filter Fonksiyonu Ile Silme Isleminden Sonra
     const afterDeletingTasks = tasks.filter((task) => {
-
-      // Silinen Task in id Bilgisi Task Listin Icinde Yoksa
-      // Task lari Farkli Bir Array Icine Aliyoruz
       return task.id !== id;
     });
-
-    // filter Fonksiyonu Ile Silme Isleminden Sonra
-    // Task larin Bulundugu Array i
-    // afterDeletingTasks Degiskenini Parametre Olarak Verip
-    // setTasks Uzerinden tasks Degiskenine Atama Yapiyoruz
     setTasks(afterDeletingTasks);
   };
-
-  // id Bilgisi Uzerinden Guncellenen Task Bilgilerini Aliyoruz
   const editTaskById = async (id, updatedTitle, updatedTaskDescription) => {
     await Axios.put(`http://localhost:3004/tasks/${id}`, {
       title: updatedTitle,
       taskDescription: updatedTaskDescription,
     });
-
-    // tasks Degiskeni Uzerinden Gelen 
-    // Task in Update Edilmis Halini 
-    // map Fonksiyonu Ile Tarama Yapiyoruz
     const updatedTasks = tasks.map((task) => {
-
-      // Update Edilmek Istenilen Task Bilgisi Daha Onceden Var Olan Bilgi Ise
       if (task.id === id) {
-
-        // Update Edilen Task Bilgilerini id Bilgisi Uzerinden
-        // Onceki Taks Bilgilerini Update Edilmis Hali Ile Degistiriyoruz
         return { id, title: updatedTitle, taskDescription: updatedTaskDescription };
       }
-
-      // Task in Update Edilmis Halini Donduruyoruz
       return task;
     });
-
-    // Task in Update Edilmis Halini
-    // setTasks Degiskenine 
-    // updatedTasks Degiskenini Parametre Olarak Veriyoruz
     setTasks(updatedTasks);
   };
 
   return (
     <div className="App">
-
-      {/* Task Creaate Sablonunu Ekrana Yazdiriyoruz */}
-      {/* TaskCreate.js Dosyasinda handleSubmit Fonksiyonundaki
-          title ve taskDecription Degiskenlerindeki Degerleri Aliyoruz*/}
       <TaskCreate onCreate={createTask} />
       <h1>Tasks</h1>
-
-      {/* TaskList.js Dosyasinda Yapilan Listeleme Isleminde
-          Eklenen Tasklari Listeliyoruz */}
       <TaskList
         tasks={tasks}
-
-        /* Silinmek Istenilen Task a Verilen id Bilgisini 
-          TaskList.js Dosyasindaki TaskList Props Icinden Cagiriyoruz
-          onDelete Metoduna Veriyoruz */
         onDelete={deleteTaskById}
-
-        /* Update Edilen Task id Bilgisini 
-          TaskList.js Dosyasindaki TaskList Props Icindeki
-          onDelete Metoduna Veriyoruz */
         onUpdate={editTaskById}
       />
     </div>
--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,21 +1,8 @@
 import { useState } from 'react';
 
-// Task Create Sablonunu Olusturuyoruz
 function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
-
-  // Form Uzerindeki Input a Girilen Data yi 
-  // useState Ile
-  // title Degiskenine Atama Yapiyoruz
-  // useState Icinde title Girilmedi Ise Input Bos Geciyoruz
   const [title, setTitle] = useState(task ? task.title : '');
-
-  // useState Icinde description Girilmedi Ise Input Bos Geciyoruz
   const [taskDescription, setTaskDescription] = useState(task ? task.taskDescription : '');
-
-  // setTitle Degiskeni Uzerinden Gelen Data yi 
-  // event Parametresi Uzerinden 
-  // handleChange Fonksiyonunun Cagirildigi Yerde
-  // Kullanilmasini Sagliyoruz
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
@@ -24,28 +11,12 @@ function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // Task Uzerindeki 
-    // Update Butonuna Tiklandiginda
-    // Update Islemi Icin Gereken Kisim Acilacak
     if (taskformUpdate) {
-
-      // Update Islemi Icin 
-      // Task in id Bilgisini Ve 
-      // Description Bilgisini Aliyoruz
       onUpdate(task.id, title, taskDescription);
-
-      // Task Uzerindeki
-      // Update Butonuna Tiklanmadiginda
-      // Task Normal Durumda Gorunecek
     } else {
       onCreate(title, taskDescription);
     }
-
-    // Task Ekleme Isleminden Sonra Input un Icini Siliyoruz
     setTitle('');
-
-    // Task Ekleme Isleminden Sonra Input un Icini Siliyoruz
     setTaskDescription('');
   };
 
@@ -53,8 +24,6 @@ function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
     <div>
       {' '}
       {taskformUpdate ? (
-
-        // Update Butonuna Tiklandiginda Gorunecek Form Duzeni
         <div className="task-update">
           <h3>Please Taskı Edit!</h3>
           <form className="task-form">
@@ -80,16 +49,10 @@ function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
           </form>
         </div>
       ) : (
-
-        // Update Butonuna Tiklanmadiginda Gorunecek Form Duzeni
         <div className="task-create">
           <h3>Please Task Add!</h3>
           <form className="task-form">
             <label className="task-label">Title</label>
-
-            {/* Input Alanina Girilen Degeri title Degiskeni Uzerinden
-                onChange Metodu Icinde 
-                handleChange Metodu Ile Aliyoruz*/}
             <input
               value={title}
               onChange={handleTitleChange}
@@ -102,8 +65,6 @@ function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
               className="task-input"
               rows={5}
             />
-
-            {/* Butona Tiklandiginda Title Ve Task Add Alanlarina Girilen Bilgileri Aliyoruz */}
             <button className="task-button" onClick={handleSubmit}>
               Create
             </button>
